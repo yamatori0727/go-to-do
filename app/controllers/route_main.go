a091d277ada08c936b6d9e867150873fb7295139
yamatori0727 <kyoto7272233@i.softbank.jp>
2021-02-23 21:17:59 +0900

    signup login logoutまで完了

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -4,6 +4,22 @@ import (
 	"net/http"
 )
 
+//HTMLの紐付けをし、表示する役割
 func top(w http.ResponseWriter, r *http.Request){
-	generateHTML(w, "Hello", "layout", "top")
+	_, err := session(w, r)
+	if err != nil{
+		generateHTML(w, "Hello", "layout","public_navbar", "top")
+	}else{
+		http.Redirect(w, r, "/todos", 302)
+	}
+
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	_, err := session(w, r)
+	if err != nil{
+		http.Redirect(w, r, "/", 302)
+	}else{
+		generateHTML(w, nil, "layout", "index", "private_navbar")
+	}
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"awesomeProject/awesomeProject/todo_app/app/models"
 	"awesomeProject/awesomeProject/todo_app/config"
 	"fmt"
 	"html/template"
@@ -16,12 +17,32 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string){
 	templates.ExecuteTemplate(w, "layout", data)
 }
 
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error){
+	cookie, err := r.Cookie("_cookie")
+	if err == nil{
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok{
+			err = fmt.Errorf("Invalid session")
+		}
+	}
+	return sess, err
+}
+
+
+
 
 
 func StartMainServer() error {
+	//静的ファイル（CSSとJS）を設定し、紐づける役割
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 
+	//urlとハンドラーの紐付けをし、返す役割
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/todos", index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
